b6932dd65f8da007032c09207eb2009e9085a327
Marius Kroh <mail@mariuskroh.de>
2020-11-27 15:58:34 +0100

    init react and tailwind starter

---
+++ src/Elevator.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Elevator = () => {
+  return (
+    <main className="bg-gray-100">
+      <div className="text-gray-900 font-sans text-xl">Test styles</div>
+    </main>
+  );
+};
+
+export default Elevator;
